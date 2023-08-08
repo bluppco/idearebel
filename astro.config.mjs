aaf9a6a88e2ae5d1a98daf2e3ac1a667f56e4b05
Mohit <mohit@hey.com>
2023-08-08 17:53:02 +0530

    x

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import inspectUrls from "@jsdevtools/rehype-url-inspector"
+import inspectUrls from "@jsdevtools/rehype-url-inspector";
 import mdx from "@astrojs/mdx";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), react()],
   markdown: {
-    rehypePlugins: [
-      [
-        inspectUrls, {
-          selectors: ["a[href]"],
-          inspectEach(url) {
-            url.node.properties.target = "_blank";
-          }
-        }
-      ]
-    ],
-  },
-})
+    rehypePlugins: [[inspectUrls, {
+      selectors: ["a[href]"],
+      inspectEach(url) {
+        url.node.properties.target = "_blank";
+      }
+    }]]
+  }
+});
