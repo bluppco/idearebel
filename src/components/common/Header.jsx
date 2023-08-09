215aa8ca56203e30730cc5f59a86e02c02cdc6ea
Mohit <mohit@hey.com>
2023-08-09 17:11:56 +0530

    x

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -7,13 +7,14 @@ const Header = () => {
     const headerRef = React.useRef( null )
     const isInView = useInView( headerRef )
     const [ switchIt, updateSwitchIt ] = React.useState( false )
-
+    const [ displayNavigation, updateDisplayNavigation ] = React.useState( false )
     React.useEffect( () => {
 
         updateSwitchIt( isInView )
 
     }, [ isInView ])
 
+    console.log( displayNavigation )
     return (
         <div ref={ headerRef } className="block">
             <section className="bg-home w-full h-screen bg-cover bg-center flex justify-center items-center bg-zinc-200 z-10">
@@ -23,18 +24,42 @@ const Header = () => {
                     /> */}
                 </div>
             </section>
-            <div className="!fixed p-8 flex justify-between w-full z-20 top-0 left-0">
-                <div className={`${ switchIt ? "text-white" : "text-black" } duration-200`}>
+            <div className={`${ displayNavigation ? "-z-10 opacity-0" : "z-30 opacity-100" } fixed p-8 flex justify-between w-full z-20 top-0 left-0 duration-200 ease-in transition`}>
+                <div className={`${ switchIt ? "text-white" : "text-black" }`}>
                     <div className="w-6 h-6">
                         <img
                             src={ switchIt ? "/nav-ir-logo-white.png" : "/nav-ir-logo-black.png"}
                         />
                     </div>
                 </div>
-                <div className="flex flex-col gap-2">
+                <div className="flex flex-col gap-2" onClick={ () => updateDisplayNavigation( true )}>
                     <div className={`${ switchIt ? "bg-white" : "bg-black" } duration-200 w-8 h-1`}/>
                     <div className={`${ switchIt ? "bg-white" : "bg-black" } duration-200 w-8 h-1`}/>
                     <div className={`${ switchIt ? "bg-white" : "bg-black" } duration-200 w-8 h-1`}/>
+                </div>
+            </div>
+            <div className={`${ displayNavigation ? "z-30 opacity-100": "-z-10 opacity-0" } fixed w-full h-screen bg-black left-0 top-0 duration-200 ease-in transition`}>
+                <div className="!fixed p-8 flex justify-between w-full z-20 top-0 left-0">
+                    <div className={`${ switchIt ? "text-white" : "text-black" } duration-200`}>
+                        <div className="w-6 h-6">
+                            <img
+                                src={ switchIt ? "/nav-ir-logo-white.png" : "/nav-ir-logo-black.png"}
+                            />
+                        </div>
+                    </div>
+                    <div className="flex flex-col gap-2" onClick={ () => updateDisplayNavigation( false )}>
+                        <div className="font-bold text-4xl text-white">x</div>
+                    </div>
+                </div>
+                <div className="flex items-center justify-center h-screen">
+                    <div className="flex flex-col gap-12 w-48 items-center">
+                        <div className="text-white text-4xl font-bold hover:underline underline-offset-[12px] cursor-pointer">Work</div>
+                        <div className="text-white text-4xl font-bold hover:underline underline-offset-[12px] cursor-pointer">About</div>
+                        <div className="text-white text-4xl font-bold hover:underline underline-offset-[12px] cursor-pointer">Services</div>
+                        <div className="text-white text-4xl font-bold hover:underline underline-offset-[12px] cursor-pointer">Careers</div>
+                        <div className="text-white text-4xl font-bold hover:underline underline-offset-[12px] cursor-pointer">Ideas</div>
+                        <div className="text-white text-4xl font-bold hover:underline underline-offset-[12px] cursor-pointer">Contact</div>
+                    </div>
                 </div>
             </div>
         </div>
