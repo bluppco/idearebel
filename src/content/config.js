dde7a03a8138bc2927b27403473e5577539dcfa2
Mohit <mohit@hey.com>
2023-08-11 17:47:39 +0530

    x

--- src/content/config.js
+++ src/content/config.js
@@ -52,11 +52,31 @@ const workCollection = defineCollection({
 	})
 
 })
+const servicesCollection = defineCollection({
+
+	schema: z.object({
+
+		description: z.string(),
+		image: z.string(),
+		link: z.string(),
+		listing_icon:  z.string(),
+		listing_title: z.string(),
+		order: z.number(),
+		seo_description: z.string(),
+		seo_title: z.string(),
+		sub_title: z.string(),
+		title: z.string(),
+		url: z.string(),
+
+	})
+
+})
 
 export const collections = {
 
 	"careers": careersCollection,
 	"ideas": ideasCollection,
+	"services": servicesCollection,
 	"work": workCollection,
 
 }
