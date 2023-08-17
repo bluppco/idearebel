301ac6954eb912d1240b512df9fb864c60593dcd
Mohit <mohit@hey.com>
2023-08-17 13:27:46 +0530

    x

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -143,10 +143,7 @@ const Header = ( props ) => {
                 </div>
                 <div className="flex flex-col gap-2" onClick={ () => updateDisplayNavigation( true )}>
                     <div className="w-7 h-7 flex items-center justify-center">
-                        <img
-                            src={ switchIt ? "/icons/menu.svg" : "/icons/menu-black.svg"}
-                            className="object-fit h-full w-full"
-                        />
+                        <svg className={`${switchIt ?"fill-white": "fill-black"} w-7 h-7`} xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
                     </div>
                 </div>
             </div>
