1da52808c6ed64898d56fcb11b1b797f9428fcb7
Mohit <mohit@hey.com>
2023-08-17 17:18:55 +0530

    x

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,13 +1,3 @@
-export const CREATIVE_IDEAS = [{
-    link: "ideas/canadas-digital-mavericks-pioneering-innovative-business-solutions/",
-    title: "Canada's Digital Mavericks Pioneering Innovative Business Solutions",
-    image: "https://blupp.b-cdn.net/idearebel/nike-digital-shoe-idea-rebel.jpeg?quality=80&width=800",
-},{
-    link: "ideas/canadas-digital-mavericks-pioneering-innovative-business-solutions/",
-    title: "AI and Digital Marketing - A Must For 2023",
-    image: "https://blupp.b-cdn.net/idearebel/AI-Marketing-2023-Digital-Agency-1.jpeg?quality=80&width=800",
-}]
-
 export const CDN_LINK = "https://blupp.b-cdn.net/idearebel/"
 export const IMAGE_QUALITY = 80
 export const IMAGE_WIDTH = 600
